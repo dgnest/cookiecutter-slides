662a1cf0439b3233c857b0a3ed74cf88b720d0df
@slovacus <slovacus@gmail.com>
2017-03-21 09:42:02 -0500

    !#2 Fix: settings multiple files

--- {{cookiecutter.repo_name}}/script/watch.sh
+++ {{cookiecutter.repo_name}}/script/watch.sh
@@ -4,4 +4,6 @@
 # shellcheck source=/dev/null
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
-cd "${SOURCE_DIR}" || exit
+cd "${SOURCE_DIR}" || echo 'path not found'
+
+landslide "${ETC_DIR}/"landslide.cfg -w ./
